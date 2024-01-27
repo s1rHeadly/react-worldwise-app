42157fdef55a846eef4714724ebd7cbf2c6399fd
davidmchale <dmchale@squiz.net>
2024-01-28 09:57:36 +1100

    basic routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+
+import {Routes, Route} from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
+
+
+
 const App = () => {
 
 
-  const number = 12;
-
   return (
-    <div>{number}</div>
+   <>
+   {/* we could have a global header here */}
+    <Routes>
+    <Route exact path="/" element={<Home />} />
+      <Route path="product" element={<Product />} />
+      <Route path="pricing" element={<Pricing />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+   {/* we could have a global footer here */}
+   </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>WorldwWise</div>
+  )
+}
+
+export default Home
