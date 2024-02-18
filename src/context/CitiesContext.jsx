6b3f08b054b9dfa6df542ce571c58745002c0c60
davidmchale <dmchale@squiz.net>
2024-02-19 09:18:59 +1100

    usefetch with Context

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -12,38 +12,9 @@ const CitiesContext = createContext();
 
 function CitiesProvider({children}){
 
-
-    const [cities, setCities] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const {data: cities, loading, error} = useFetch(`${BASE_URL}/cities`)
     const [currentCity, setCurrentCity] = useState({});
     
-
-
-    useEffect(() => {
-      const citiesData = async(url) => {
-        setLoading(true)
-        try {
-          const response = await fetch(url);
-          if(!response.ok) return
-          
-          if(response.status === 200){
-            const data = await response.json();
-            setCities(data)
-            
-          }
-        
-        } catch (error) {
-          setError(error)
-          setLoading(true)
-        }finally{
-          setLoading(false)
-        }
-      }
-
-      citiesData(`${BASE_URL}/cities`)
-
-    }, []);
 
   
    
